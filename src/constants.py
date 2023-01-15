3670082c21dced148d8091e0d0d14d9d58606bad
PietroAnsidei <pansidei@yahoo.it>
2023-01-15 15:44:17 +0100

    add pickup

--- src/constants.py
+++ src/constants.py
@@ -11,6 +11,7 @@ params = {
     "reload": True,
     "data_sheet": "Stops 1.Tour (Mo-Fr)",
     "only_stores": False,
+    "only_delivery": False,
     # Coordinates of the production centre at Gewerbegebiet Südstraße 5
     "start_loc": ["13.5853717,51.6286970"],
     "osrm_params": {"annotations": "distance,duration"},
@@ -25,12 +26,13 @@ params = {
     # Maximum execution time
     "max_search_time_min": 10,
     # Requirements
-    "num_vehicles": 7,
-    "max_legs": 4.4,  # Allow 4 store-legs (1) + 4 customer legs (0.1)
+    "num_vehicles": 10,
+    "max_legs": 4.44,  # Allow 4 store- (1), 4 customer- (0.1), 4 pickup-legs(0.01)
     "leave_time": time(2, 45),
     "max_time_tour_h": 7,
+    "pickup_delay_h": 0,  # Allow max delay on the ETA (Time to) for pickup
     # Other parameters
     "max_distance_vehicles_km": 1000,  # For each vehicle's route
     "pen_distance_vehicle_factor": 0,  # To penalize the max of the routes lengths
-    "slack_time_max_h": 1,  # Max allowed stopping time at location
+    "slack_time_max_h": 0.5,  # Max allowed stopping time at location + min pickup delay
 }
